7090940d00c984f5971bdbfd491f111257229bbe
ruanshr <946843850@qq.com>
2023-04-17 22:35:38 +0800

    choic: react + vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import { useState } from 'react'
-import './App.css'
-
+import { useState } from "react";
+import "./App.css";
+import { Button } from "antd";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        count is {count}
+        <Button type="primary" onClick={() => setCount((count) => count + 1)}>
+          点击
+        </Button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { createStyleImportPlugin, AntdResolve } from "vite-plugin-style-import";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    createStyleImportPlugin({
+      resolves: [AntdResolve()],
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          // 更改主题在这里
+          "primary-color": "#52c41a",
+          "link-color": "#1DA57A",
+          "border-radius-base": "2px",
+        },
+        javascriptEnabled: true,
+      },
+    },
+  },
+});
