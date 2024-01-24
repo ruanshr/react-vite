c3f796d48c06478a7b4eff36eaf79373bf065910
ruanshr <946843850@qq.com>
2024-01-24 22:54:53 +0800

    feat: 测试1

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+console.log("222")
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
     <App />
